4a30a28fc3a23bc82cb69b834202823b11819f71
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-11 22:46:48 -0500

    feat: estructura inicial de paquetes y clases vacias (model, dao, service, view)

---
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/view/VentanaLogin.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventarios.valledupar.view;
+
+/**
+ *
+ * @author Bayron
+ */
+public class VentanaLogin {
+    
+}
+a
