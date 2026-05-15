a6426bddf1f45029c092ad1017f97dd5118ad921
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-15 15:10:27 -0500

    feat: clase Alerta para notificaciones de stock bajo minimo

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/model/Alerta.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/model/Alerta.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.model;
 
-/**
- *
- * @author Bayron
- */
 public class Alerta {
-    
+
+    private int idAlerta;
+    private int idProducto;
+    private String fechaAlerta;
+    private int stockAlMomento;
+    private String estadoAlerta;
+
+    public Alerta() {}
+
+    public Alerta(int idAlerta, int idProducto, String fechaAlerta,
+                  int stockAlMomento, String estadoAlerta) {
+        this.idAlerta = idAlerta;
+        this.idProducto = idProducto;
+        this.fechaAlerta = fechaAlerta;
+        this.stockAlMomento = stockAlMomento;
+        this.estadoAlerta = estadoAlerta;
+    }
+
+    public int getIdAlerta() { return idAlerta; }
+    public void setIdAlerta(int idAlerta) { this.idAlerta = idAlerta; }
+
+    public int getIdProducto() { return idProducto; }
+    public void setIdProducto(int idProducto) { this.idProducto = idProducto; }
+
+    public String getFechaAlerta() { return fechaAlerta; }
+    public void setFechaAlerta(String fechaAlerta) { this.fechaAlerta = fechaAlerta; }
+
+    public int getStockAlMomento() { return stockAlMomento; }
+    public void setStockAlMomento(int stockAlMomento) { this.stockAlMomento = stockAlMomento; }
+
+    public String getEstadoAlerta() { return estadoAlerta; }
+    public void setEstadoAlerta(String estadoAlerta) { this.estadoAlerta = estadoAlerta; }
+
+    public void marcarAtendida() {
+        this.estadoAlerta = "atendida";
+    }
+
+    public boolean esPendiente() {
+        return "pendiente".equalsIgnoreCase(this.estadoAlerta);
+    }
 }
