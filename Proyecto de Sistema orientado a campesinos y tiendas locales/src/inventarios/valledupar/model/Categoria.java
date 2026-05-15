d29ba1c9185fbc1135f3a040b76b8a13a0133ef5
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-14 23:52:04 -0500

    feat: implementar clase Categoria

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/model/Categoria.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/model/Categoria.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.model;
 
-/**
- *
- * @author Bayron
- */
 public class Categoria {
-    
+
+    private int idCategoria;
+    private String nombre;
+    private String descripcion;
+    private String estado;
+
+    public Categoria() {}
+
+    public Categoria(int idCategoria, String nombre, String descripcion, String estado) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public int getIdCategoria() { return idCategoria; }
+    public void setIdCategoria(int idCategoria) { this.idCategoria = idCategoria; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
 }
