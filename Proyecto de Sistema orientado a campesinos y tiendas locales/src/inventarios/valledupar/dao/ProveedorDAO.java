c50336b9e45fe450cdbc46bac602ea00bb3cf658
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-16 17:14:31 -0500

    feat: clase ProveedorDAO con estructura lista para implementacion

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/ProveedorDAO.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/ProveedorDAO.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.dao;
 
-/**
- *
- * @author Bayron
- */
-public class ProveedorDAO {
-    
+import inventarios.valledupar.model.Proveedor;
+import java.util.List;
+
+public class ProveedorDAO implements IProveedorDAO {
+
+    @Override
+    public void guardar(Proveedor proveedor) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Proveedor buscarPorId(int idProveedor) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Proveedor> buscarTodos() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void actualizar(Proveedor proveedor) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void eliminar(int idProveedor) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
