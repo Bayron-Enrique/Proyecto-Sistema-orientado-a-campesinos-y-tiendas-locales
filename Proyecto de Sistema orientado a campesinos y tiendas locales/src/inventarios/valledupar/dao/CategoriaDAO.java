1623e4d2704817dcedab836849326feb883e8a34
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-16 00:13:43 -0500

    feat: clase CategoriaDAO con estructura lista para implementacion

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/CategoriaDAO.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/CategoriaDAO.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.dao;
 
-/**
- *
- * @author Bayron
- */
-public class CategoriaDAO {
-    
+import inventarios.valledupar.model.Categoria;
+import java.util.List;
+
+public class CategoriaDAO implements ICategoriaDAO {
+
+    @Override
+    public void guardar(Categoria categoria) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Categoria buscarPorId(int idCategoria) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Categoria> buscarTodas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void actualizar(Categoria categoria) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void eliminar(int idCategoria) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
