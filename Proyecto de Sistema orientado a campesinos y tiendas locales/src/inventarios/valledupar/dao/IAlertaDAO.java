29ab74cdb3ff3c121f506bac73159bddc36350dc
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-15 23:59:28 -0500

    feat: interfaz IAlertaDAO para gestion de alertas de stock

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/IAlertaDAO.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/IAlertaDAO.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.dao;
 
-/**
- *
- * @author Bayron
- */
-public class IAlertaDAO {
-    
+import inventarios.valledupar.model.Alerta;
+import java.util.List;
+
+public interface IAlertaDAO {
+
+    void guardar(Alerta alerta);
+    List<Alerta> buscarPendientes();
+    List<Alerta> buscarTodas();
+    void marcarAtendida(int idAlerta);
 }
