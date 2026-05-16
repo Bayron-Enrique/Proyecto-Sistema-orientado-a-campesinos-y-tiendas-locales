7526e50c992b9267810bb0d2810f8289f7bc3952
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-15 20:10:03 -0500

    feat: interfaz ICategoriaDAO con operaciones CRUD

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/ICategoriaDAO.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/ICategoriaDAO.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.dao;
 
-/**
- *
- * @author Bayron
- */
-public class ICategoriaDAO {
-    
+import inventarios.valledupar.model.Categoria;
+import java.util.List;
+
+public interface ICategoriaDAO {
+
+    void guardar(Categoria categoria);
+    Categoria buscarPorId(int idCategoria);
+    List<Categoria> buscarTodas();
+    void actualizar(Categoria categoria);
+    void eliminar(int idCategoria);
 }
