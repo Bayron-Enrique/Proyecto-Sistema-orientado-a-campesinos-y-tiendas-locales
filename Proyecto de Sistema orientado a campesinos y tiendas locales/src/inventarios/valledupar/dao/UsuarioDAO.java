33ee74d54e150c806e7edc366fd36341dbffef1f
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-16 00:04:52 -0500

    feat: clase UsuarioDAO con estructura lista para implementacion

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/UsuarioDAO.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/UsuarioDAO.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inventarios.valledupar.dao;
 
-/**
- *
- * @author Bayron
- */
-public class UsuarioDAO {
-    
+import inventarios.valledupar.model.Usuario;
+import java.util.List;
+
+public class UsuarioDAO implements IUsuarioDAO {
+
+    @Override
+    public void guardar(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Usuario buscarPorId(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Usuario> buscarTodos() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void actualizar(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void eliminar(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Usuario autenticar(String correo, String contrasena) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
