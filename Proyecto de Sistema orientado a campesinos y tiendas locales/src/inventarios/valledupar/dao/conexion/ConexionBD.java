c9b77aeda62266b57ab8d0fddcb04c37950ce5d7
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-05-31 23:57:27 -0500

    refactor: cambio en la conexion de la base de datos

--- Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/conexion/ConexionBD.java
+++ Proyecto de Sistema orientado a campesinos y tiendas locales/src/inventarios/valledupar/dao/conexion/ConexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USUARIO = "system";
+private static final String USUARIO = "inventarios";
 private static final String CONTRASENA = "1234";
 
     private static Connection conexion = null;
